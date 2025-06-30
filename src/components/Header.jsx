3a07b0e7a208d403c90c15652799309ff116be7c
devclarova <dev.clarova@gmail.com>
2025-06-30 18:09:31 +0900

    [docs] JSX 기초

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// css
+// import "../css/Header.css";
+import styles from "../css/Header.module.css";
+
+function Header() {
+  return <div className={styles.Header}>Header</div>;
+}
+
+export default Header;
