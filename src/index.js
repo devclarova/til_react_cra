560c1ce69e3d7f6e57ae9ac9e599ca42db4074ad
dev.clarova <dev.clarova@gmail.com>
2025-07-15 15:50:17 +0900

    [react-router-dom] 설치 및 기초 이해

--- src/index.js
+++ src/index.js
@@ -3,23 +3,11 @@ import ReactDOM from "react-dom/client";
 
 // css
 import "./index.css";
-import StartPage from "./pages/StartPage";
-import Todos from "./pages/Todos";
-import Users from "./pages/Users";
-import Photos from "./pages/Photos";
-import Posts from "./pages/Posts";
-import Albums from "./pages/Albums";
-import Comments from "./pages/Comments";
-import Test from "./pages/Test";
-import RegisterPage from "./pages/RegisterPage";
-import Todo from "./pages/Todo";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const Hello = function () {
-  return <div>안녕하세요.</div>;
-};
 root.render(
   <div className="wrap">
-    <Todo />
+    <App />
   </div>,
 );
